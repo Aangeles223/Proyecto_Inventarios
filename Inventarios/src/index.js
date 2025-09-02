1fa53ebc529d977ce612f6c52bd0ef971c711eef
Aangeles223 <122044096@upq.edu.mx>
2025-09-01 21:16:02 -0600

    creacion de Barrel

---
+++ Inventarios/src/index.js
@@ -0,0 +1,10 @@
+export { default as App } from "./App.jsx";
+export { default as AuthContext } from "./context/AuthContext.jsx";
+export * from "./hooks";
+export * from "./pages";
+export * from "./routers";
+export * from "./store";
+export * from "./styles";
+export * from "./supabase/supabase.config.jsx";
+export * from "./utils/dataEstatica.jsx";
+// Nota: No se exportan archivos CSS ni assets, solo módulos JS/JSX.
