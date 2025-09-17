bc197844b6af9388c8d84b9c4acf91c9f35b07c2
Aangeles223 <122044096@upq.edu.mx>
2025-09-16 18:30:29 -0600

    finla

--- Inventarios/src/routers/routes.jsx
+++ Inventarios/src/routers/routes.jsx
@@ -1,17 +1,50 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "../index";
+import {
+  Configuracion,
+  ErrorMolecula,
+  Home,
+  Login,
+  Marca,
+  ProtectedRoute,
+  SpinnerLoader,
+  UserAuth,
+  useEmpresaStore,
+  useUsuariosStore,
+  Categorias,
+  Productos,
+  Usuarios
+} from "../index";
+import { useQuery } from "@tanstack/react-query";
+
 export function MyRoutes() {
-  return ( 
-      <Routes>
+  const { user } = UserAuth();
+  const { mostrarUsuarios,idusuario,mostrarpermisos } = useUsuariosStore();
+  const {mostrarEmpresa} = useEmpresaStore()
+  const { data:datausuarios, isLoading, error } = useQuery({
+    queryKey: ["mostrar usuarios"],
+    queryFn: mostrarUsuarios,
+  });
+  const {data:dataempresa}=useQuery({queryKey:["mostrar empresa"],queryFn:()=>mostrarEmpresa({idusaurio:idusuario}),enabled:!!datausuarios})
+  const {data:datapermisos}=useQuery({queryKey:["mostrar permisos",{id_usuario:idusuario}],queryFn:()=>mostrarpermisos({id_usuario:idusuario}),enabled:!!datausuarios})
+
+  if (isLoading){
+    return <SpinnerLoader/>
+  }
+  if(error){
+    return <ErrorMolecula mensaje={error.message}/>
+  }
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route element={<ProtectedRoute user={user} redirectTo="/login" />}>
         <Route path="/" element={<Home />} />
-      </Routes>
-  );
-}import { Routes, Route } from "react-router-dom";
-import { Home } from "../index";
-export function MyRoutes() {
-  return ( 
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <Route path="/configurar" element={<Configuracion />} />
+        <Route path="/configurar/marca" element={<Marca />} />
+        
+        <Route path="/configurar/categorias" element={<Categorias />} />
+        <Route path="/configurar/productos" element={<Productos />} />
+        <Route path="/configurar/personal" element={<Usuarios />} />
+      </Route>
+    </Routes>
   );
 }
